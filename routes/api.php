25e3819b6b59eb779ff152c0a80b4edc038cf07f
Cédric <limacedric@hotmail.fr>
2024-07-20 17:57:38 +0200

    add controller media + route api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\MediaPlanController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Display a listing of the resource.
+ * Préfixe de l'URI : /api
+ * https://laravel.com/docs/11.x/routing
+ */
+Route::apiResource('media', MediaPlanController::class)->middleware('auth:sanctum');
